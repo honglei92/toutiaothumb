3990a91530b8b6c44395093d6dc44e40bdd930c8
王宏雷 <wanghonglei@360tianma.com>
2021-02-10 13:58:23 +0800

    增加长按事件

--- app/src/main/java/com/whl/toutiaothumb/MainActivity.java
+++ app/src/main/java/com/whl/toutiaothumb/MainActivity.java
@@ -1,8 +1,11 @@
 package com.whl.toutiaothumb;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,6 +16,18 @@ import com.whl.toutiaothumb.View.ArticleRl;
 public class MainActivity extends AppCompatActivity {
     private ImageView ivThumb, ivThumbBottom;
     private ArticleRl articleThumbRl;
+    final Handler handler = new Handler();
+    final Runnable mLongPressed = new Runnable() {
+        @Override
+        public void run() {
+            articleThumbRl.setVisibility(View.VISIBLE);
+            articleThumbRl.setThumb(true, x, y, articleThumbRl);
+            handler.postDelayed(mLongPressed, 100);
+        }
+    };
+    private int x;
+    private int y;
+    private long lastDownTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 articleThumbRl.setThumb(true, x, y, articleThumbRl);
             }
         });
+/*
         ivThumbBottom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +57,30 @@ public class MainActivity extends AppCompatActivity {
                 int y = location[1]; // view距离window 顶边的距离（即y轴方向）
                 articleThumbRl.setVisibility(View.VISIBLE);
                 articleThumbRl.setThumb(true, x, y, articleThumbRl);
+            }
+        });
+*/
+        ivThumbBottom.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDownTime = System.currentTimeMillis();
+                    x = (int) event.getRawX();
+                    y = (int) event.getRawY();
+                    Log.i("aaa", (System.currentTimeMillis() - lastDownTime) + "");
+                    handler.postDelayed(mLongPressed, 100);
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Log.i("aaa", (System.currentTimeMillis() - lastDownTime) + "");
+                    if (System.currentTimeMillis() - lastDownTime < 100) {//判断为单击事件
+                        articleThumbRl.setVisibility(View.VISIBLE);
+                        articleThumbRl.setThumb(true, x, y, articleThumbRl);
+                        handler.removeCallbacks(mLongPressed);
+                    } else {//判断为长按事件后松开
+                        handler.removeCallbacks(mLongPressed);
+                    }
+                }
+                return true;
             }
         });
     }
